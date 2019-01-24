23fb601962fd527f426f74ac5b5a1ef3ee6f6715
travis <trav.horn@gmail.com>
2019-01-23 21:06:29 -0700

    pt 1

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-
+console.log("Hello World!");
 
 /** 2) A first working Express Server */
 
@@ -24,8 +24,8 @@ var app = express();
 
 
 /** 6) Use the .env file to configure the app */
- 
- 
+
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
@@ -39,7 +39,7 @@ var app = express();
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
